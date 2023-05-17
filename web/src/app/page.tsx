c08d9105c5d6a3f56027791da7209eb298cf1f7c
Gael <rchsilver@gmail.com>
2023-05-17 15:35:05 -0300

    feat: add colors var in tailwind config

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
+      <h1 className="text-4xl font-bold">Hello</h1>
+      <h1 className="text-4xl font-alt">Hello</h1>
+    </div>
+  );
 }
