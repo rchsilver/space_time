e609b2fa6704c6feff01f0249569cd4dc315c468
Gael <rchsilver@gmail.com>
2023-05-19 08:56:29 -0300

    feat: add generate token jtw and access of github

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,27 @@
+import { Copyright } from "@/components/home/left/Copyright";
+import { EmptyMemories } from "@/components/home/right/EmptyMemories";
+import { Hero } from "@/components/home/left/Hero";
+import { SignIn } from "@/components/home/left/SignIn";
+import { Blur } from "@/components/home/left/Blur";
+import { Stripes } from "@/components/home/left/Stripes";
+
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
-      <h1 className="text-4xl font-bold">Hello</h1>
-      <h1 className="text-4xl font-alt">Hello</h1>
-    </div>
+    <main className="grid grid-cols-2 min-h-screen">
+      {/* left */}
+      <section className="relative flex flex-col items-start justify-between bg-[url('../assets/bg-stars.svg')] bg-cover px-28 py-16 overflow-hidden border-r border-white/10 ">
+        <Blur />
+        <Stripes />
+
+        <SignIn />
+        <Hero />
+        <Copyright />
+      </section>
+
+      {/* right */}
+      <section className="flex flex-col bg-[url('../assets/bg-stars.svg')] bg-cover p-16">
+        <EmptyMemories />
+      </section>
+    </main>
   );
 }
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         sans: "var(--font-roboto)",
         alt: "var(--font-bai-jamjuree)",
       },
+
       colors: {
         gray: {
           50: "#eaeaea",
@@ -72,6 +73,22 @@ module.exports = {
           800: "#027435",
           900: "#025929",
         },
+      },
+
+      blur: {
+        full: "194px",
+      },
+
+      backgroundImage: {
+        stripes:
+          "linear-gradient(to bottom, rgb(255, 255, 255, 0.1), rgb(255, 255, 255, 0.1) 12.5%, transparent 12.5%, transparent)",
+      },
+
+      backgroundSize: {
+        stripes: "100% 8px",
+      },
+      fontSize: {
+        "5xl": "2.5rem",
       },
     },
   },
